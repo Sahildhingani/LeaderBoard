f4ed056c9082e5a4db5d9d1dc1440830c01e4914
sahil dhingai <sahildhingani51@gmail.com>
2025-01-04 15:41:13 +0530

    update the redux

--- frontend/src/redux/slice.js
+++ frontend/src/redux/slice.js
@@ -2,12 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     users: [
-        { id: 'abc123', name: 'Aman', totalSolved: 50 },
-        { id: 'def456', name: 'Kunal', totalSolved: 120 },
-        { id: 'ghi789', name: 'Rohit', totalSolved: 75 },
-        { id: 'jkl012', name: 'Akash', totalSolved: 200 },
-        { id: 'mno345', name: 'Kartik', totalSolved: 90 },
-        { id: 'pqr678', name: 'Parag', totalSolved: 130 },
+        
     ],
 };
 
